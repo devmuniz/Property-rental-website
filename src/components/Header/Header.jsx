2a807d39190f8afe57cbad54b9f64905eb75ba27
Gustavo Muniz <gustavomunizdr@hotmail.com>
2024-12-07 21:31:30 -0300

    A menu header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,14 @@ function Header () {
 
                     <a href="/"><img src={"../src/assets/img/logo.png"} className='logo'/></a>
                     <h2 className='titleHeader'>Anuncie seu espaço e fature!</h2>
+
+                    <div>
+                    <a href=""><img src={"../src/assets/img/building.png"}  className='userIcon' alt="" /></a>
+                    <a href=""><img src={"../src/assets/img/calendar.png"}  className='userIcon' alt="" /></a>
+                    <a href=""><img src={"../src/assets/img/table.png"}  className='userIcon' alt="" /></a>
+                    <a href=""><img src={"../src/assets/img/chat.png"}  className='userIcon' alt="" /></a>
                     <a href=""><img src={"../src/assets/img/user.png"}  className='userIcon' alt="" /></a>
+                    </div>
                 
             </div>
  
